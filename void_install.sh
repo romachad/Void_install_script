548c62a7d1ebb24b02990fa6c0bc0c8efc5e9d64
Coret <coret@cf.net>
2022-09-23 16:45:07 -0300

    First version of script to validade

---
+++ void_install.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+# == MY VOID SETUP INSTALLER By XYZ == #
+
+
+#INSERT VARIABLES HERE!
+#######################
+BTRFS_OPTS="rw,noatime,ssd,compress=zstd,space_cache,commit=120"
+#######################
+
+#part1
+printf '\033c'
+echo "Welcome to romachad moded void installer script"
+#sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 15/" /etc/pacman.conf
+#pacman --noconfirm -Sy
+loadkeys br-abnt2
+# timedatectl set-ntp true
+lsblk
+echo -e "Choose the drive to install the VOID.\n\nChoose GPT table and create 3 partitions:"
+echo -e "1-> EFI patition with size of at lease +200M\n2-> Boot partition of at least +800M (this one as Linux filesystem)\n3-> The remainder as Linux filesystem"
+echo -e "\nChoose disk:"
+read drive
+fdisk /dev/$drive
+t_part=$(lsblk | grep $drive|grep -v "^$drive"|wc -l)
+[ $t_part -ne 3 ] && echo -e "Its necessary 3 partitions! The $drive has only $t_part!\nStart the script again and fix this in the fdisk!" && exit 1;
+
+efi_part=$(fdisk -l /dev/$drive | grep "${drive}1"|grep EFI|wc -l)
+[ $efi_part -ne 1 ] && echo -e "There should be ONE EFI partition it was found: $efi_part!\nStart the script again and fix this in the fdisk!" && exit 1;
+
+#Create the file systems!
+mkfs.vfat -nBOOT -F32 /dev/${drive}1
+mkfs.ext2 -L grub /dev/${drive}2
+
+echo "Creating LUKS-encrypted root partition."
+cryptsetup luksFormat --type=luks -s=512 /dev/${drive}3
+echo "Type the password again to open the partition:"
+cryptsetup open /dev/${drive}3 cryptroot
+mkfs.btrfs -L void /dev/mapper/cryptroot
+echo "Partitions created"
+#Partitions created.
+
+#Mounting the partitions
+mount -o $BTRFS_OPTS /dev/mapper/cryptroot /mnt
+btrfs subvolume create /mnt/@
+btrfs subvolume create /mnt/@home
+btrfs subvolume create /mnt/@snapshots
+umount /mnt
+
+mount -o $BTRFS_OPTS,subvol=@ /dev/mapper/cryptroot /mnt
+
+mkdir /mnt/home
+mount -o $BTRFS_OPTS,subvol=@home /dev/mapper/cryptroot /mnt/home
+
+mkdir /mnt/.snapshots
+mount -o $BTRFS_OPTS,subvol=@snapshots /dev/mapper/cryptroot /mnt/.snapshots
+
+#Mounting nested partitions which wont have a snapshot taken:
+mkdir -p /mnt/var/cache
+btrfs subvolume create /mnt/var/cache/xbps
+btrfs subvolume create /mnt/var/tmp
+btrfs subvolume create /mnt/var/srv
+
+mkdir /mnt/efi
+mount -o rw,noatime /dev/${drive}1 /mnt/efi
+
+mkdir /mnt/boot
+mount -o rw,noatime /dev/${drive}2 /mnt/boot
+#End of mount
